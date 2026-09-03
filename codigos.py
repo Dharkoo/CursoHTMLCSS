da843cff45584e633e990802102353045f67f4b6
Dharkoo <miguel.jaramillo731@gmail.com>
2026-09-03 06:49:53 -0500

    agosto

---
+++ codigos.py
@@ -0,0 +1,28 @@
+
+#mkdir proyecto
+#cd proyecto
+#python -m venv entorno
+#source entorno\bin\activate
+#pip install django
+#django-admin startproject config . 
+#python manage.py startapp usuarios
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py createsuperuser
+#python manage.py runserver
+
+
+
+
+
+
+
+#sudo apt update
+
+#sudo apt install python3 python3-pip python3-venv
+#mkdir mi_proyecto
+#cd mi_proyecto
+#python3 -m venv env
+#source env/bin/activate
+#pip install django
+#django-admin --version
